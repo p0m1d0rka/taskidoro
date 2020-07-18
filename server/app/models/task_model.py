b9a252a7a4f9044fecbf1a7b090a81d475de8e16
pomidor <vladimirelizarov89@gmail.com>
2020-07-18 22:57:01 +0300

    add create task

--- server/app/models/task_model.py
+++ server/app/models/task_model.py
@@ -2,8 +2,16 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
-class Task(BaseModel):
+class TaskModelBase(BaseModel):
     title: str
     author: str
     text: Optional[str] = None
     created_at: datetime
+
+
+class TaskModelIn(TaskModelBase):
+    pass
+
+
+class TaskModelOut(TaskModelBase):
+    _id: str
--- server/app/routes/tasks.py
+++ server/app/routes/tasks.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter
 from ..config.db import db_instance 
-from ..models.task import Task
+from ..models.task_model import TaskModelIn, TaskModelOut
 router = APIRouter()
 
+collection = db_instance.task
+
 @router.get("/tasks", tags=["tasks"])
-async def tasks():
+async def find_all_tasks():
     result = await db_instance.test_collection.insert_one({"test": "doc"})
     return {"status": "ok", "result": repr(result)}
+
+@router.get("/task/{_id}")
+async def find_one_task(_id: str = None):
+    result = await collection.find_one({"_id": _id})
+    return result 
+
+@router.post("/task")
+async def create_task(task: TaskModelIn):
+    try:
+        result = await collection.insert_one(dict(task))
+        return {"status": "success", "_id": str(result.inserted_id)}
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
